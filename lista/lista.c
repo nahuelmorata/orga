4d1f4c75d4b6a70b407451bf8f086266ca0cdf20
Agustin Montero <agus91997@gmail.com>
2019-09-11 18:01:26 -0300

    Metodos de lista

--- lista/lista.c
+++ lista/lista.c
@@ -1,1 +1,72 @@
- 
+#include "lista.h"
+#include <stdlib.h>
+
+/**
+ Inserta el elemento E, en la posición P, en L.
+ Con L = A,B,C,D y la posición P direccionando C, luego:
+ L' = A,B,E,C,D
+**/
+void l_insertar(tLista l, tPosicion p, tElemento e){
+    tPosicion nueva_celda = (tPosicion) malloc(sizeof(struct celda));
+    nueva_celda -> elemento = e;
+    nueva_celda -> siguiente = p -> siguiente;
+    p -> siguiente = nueva_celda;
+}
+
+
+/**
+ Destruye la lista L, elimininando cada una de sus celdas. Los elementos almacenados en las celdas son eliminados mediante la función fEliminar parametrizada.
+**/
+void l_destruir(tLista * l, void (*fEliminar)(tElemento)){
+    tPosicion pos_actual = *l; //primera posicion de la lista
+    tPosicion pos_aux = pos_actual ->siguiente;//guarda la posicion siguiente
+
+    //elimina el centinela
+    pos_actual->siguiente = NULL;
+    free(pos_actual);
+    pos_actual = pos_aux;
+
+    //elimina el resto de la lista
+    while(pos_actual != NULL) {
+        pos_aux = pos_actual ->siguiente;
+        fEliminar(pos_actual ->elemento);
+        pos_actual->elemento = NULL;
+        pos_actual->siguiente = NULL;
+        free(pos_actual);
+        pos_actual = pos_aux;
+    }
+}
+
+
+/**
+ Recupera y retorna la primera posición de L.
+ Si L es vacía, primera(L) = ultima(L) = fin(L).
+**/
+tPosicion l_primera(tLista l){
+    return l;
+}
+
+
+/**
+ Recupera y retorna la posición anterior a P en L.
+ Si P es primera(L), finaliza indicando LST_NO_EXISTE_ANTERIOR.
+**/
+tPosicion l_anterior(tLista l, tPosicion p){
+    if(l_primera(l) == p)
+        exit(LST_NO_EXISTE_ANTERIOR);
+    else
+        return p;
+}
+
+
+ /**
+ Recupera y retorna la posición fin de L.
+ Si L es vacía, primera(L) = ultima(L) = fin(L).
+**/
+tPosicion l_fin(tLista l){
+    tPosicion pos_actual = l; //primera posicion de la lista
+    while(pos_actual ->siguiente != NULL){
+        pos_actual = pos_actual->siguiente;
+    }
+    return pos_actual;
+}
