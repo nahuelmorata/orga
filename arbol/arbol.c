9d507381bfd1dd35e7fbb22dd8fdf4e09bd556e3
Nahuel Morata <nahuelmorata@gmail.com>
2019-09-30 23:58:58 -0300

    Agrego mis parte de funciones

--- arbol/arbol.c
+++ arbol/arbol.c
@@ -1,33 +1,100 @@
 #include "arbol.h"
+#include <stdlib.h>
 
+void (*fEliminarUsuario)(tElemento);
 
-/**
-Crea la raiz de A.
-Si A no es vacio, finaliza indicando ARB_OPERACION_INVALIDA.
-**/
-extern void crear_raiz(tArbol a, tElemento e);
+tPosicion es_hijo_de(tNodo nodoPadre, tNodo nodoHermano);
+void eliminar_nodo(tElemento elemento);
 
-/**
- Inserta y retorna un nuevo nodo en A.
- El nuevo nodo se agrega en A como hijo de NP, hermano izquierdo de NH, y cuyo rotulo es E.
- Si NH es NULL, el nuevo nodo se agrega como primer hijo de NP.
- Si NH no corresponde a un nodo hijo de NP, finaliza indicando ARB_POSICION_INVALIDA.
- NP direcciona al nodo padre, mientras NH al nodo hermano derecho del nuevo nodo a insertar.
-**/
-extern tNodo a_insertar(tArbol a, tNodo np, tNodo nh, tElemento e);
+void crear_raiz(tArbol arbol, tElemento elemento) {
+    if (arbol->raiz == NULL) {
+        exit(ARB_OPERACION_INVALIDA);
+    }
+
+    arbol->raiz = (tNodo) malloc(sizeof(struct nodo));
+
+    arbol->raiz->elemento = elemento;
+    arbol->raiz->padre = NULL;
+    crear_lista(&arbol->raiz->hijos);
+}
+
+tNodo a_insertar(tArbol arbol, tNodo nodoPadre, tNodo nodoHermano, tElemento elemento) {
+    tPosicion posicionNodoHermano = l_primera(nodoPadre->hijos);
+
+    if (nodoHermano != NULL) {    
+        posicionNodoHermano = es_hijo_de(nodoPadre, nodoHermano);
+
+        if (posicionNodoHermano != NULL) {
+            exit(ARB_POSICION_INVALIDA);
+        }
+    }
+
+    tNodo nodoNuevo = (tNodo) malloc(sizeof(struct nodo));
+    nodoNuevo->padre = nodoPadre;
+    nodoNuevo->elemento = elemento;
+    crear_lista(&nodoNuevo->hijos);
+
+    l_insertar(nodoPadre->hijos, posicionNodoHermano, nodoNuevo);
+
+    return nodoNuevo;
+}
 
 /**
  Destruye el arbol A, elimininando cada uno de sus nodos.
  Los elementos almacenados en el arbol son eliminados mediante la funcion fEliminar parametrizada.
 **/
-extern void a_destruir(tArbol * a, void (*fEliminar)(tElemento));
+void a_destruir(tArbol * a, void (*fEliminar)(tElemento)) {
+    fEliminarUsuario = fEliminar;
+    
+    tLista hijos = (*a)->raiz->hijos;
+    l_destruir(&hijos, &eliminar_nodo);
 
-/**
-Recupera y retorna el nodo correspondiente a la raiz de A.
-**/
-extern tNodo a_raiz(tArbol a);
+    fEliminar((*a)->raiz->elemento);
+    (*a)->raiz->elemento = NULL;
+
+    free((*a)->raiz);
+    (*a)->raiz = NULL;
+
+    free((*a));
+    a = NULL;
+
+    fEliminarUsuario = NULL;
+}
+
+tNodo a_raiz(tArbol a) {
+    return a->raiz;
+}
 
 /**
  Obtiene y retorna una lista con los nodos hijos de N en A.
 **/
-extern tLista a_hijos(tArbol a, tNodo n);
+tLista a_hijos(tArbol arbol, tNodo nodo) {
+    return nodo->hijos;
+}
+
+// Funciones privadas
+
+tPosicion es_hijo_de(tNodo nodoPadre, tNodo nodoHermano) {
+    tPosicion cursor = l_primera(nodoPadre->hijos);
+
+    while (cursor != NULL) {
+        tNodo hijo = (tNodo) l_recuperar(nodoPadre->hijos, cursor);
+
+        if (hijo == nodoHermano) {
+            return cursor;
+        }
+    }
+
+    return NULL;
+}
+
+void eliminar_nodo(tElemento elemento) {
+    tNodo nodo_borrar = (tNodo) elemento;
+
+    l_destruir(&nodo_borrar->hijos, &eliminar_nodo);
+    fEliminarUsuario(nodo_borrar->elemento);
+    nodo_borrar->elemento = NULL;
+    nodo_borrar->padre = NULL;
+
+    free(nodo_borrar);
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista/lista_test.c"
+
+int main() {
+    printf("Hello world!\n");
+    return 0;
+}
