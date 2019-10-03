246fc0f226aefed0878256765d52f7f10ae63d83
Agustin Montero <agus91997@gmail.com>
2019-10-02 23:34:02 -0300

    Agrego mis funciones
    - La condicion al pricipio de crear_raiz me parece que estaba mal asi que la cambie, mirala que onda.
    - Agregué los comentarios del header para ubicarme mas fácil
    - Cambie el nombre de la funcion "es_hijo_de" a "obtenerPosicion"
    - No probé nada

--- arbol/arbol.c
+++ arbol/arbol.c
@@ -3,12 +3,33 @@
 
 void (*fEliminarUsuario)(tElemento);
 
-tPosicion es_hijo_de(tNodo nodoHermano);
+// Declaracion de funciones privadas
+
+tPosicion obtenerPosicion(tNodo n);
 void eliminar_nodo(tElemento elemento);
 tNodo crear_nodo(tNodo nodoPadre, tElemento elemento);
+void fEliminarAux(tElemento e);
 
+// -----------------------
+
+/**
+Inicializa un arbol vacio.
+Una referencia al arbol creado es referenciado en *A.
+**/
+void crear_arbol(tArbol * a){
+    *a = (tArbol) malloc(sizeof(struct arbol));
+    if (*a == NULL)
+        exit(ARB_ERROR_MEMORIA);
+
+    (*a)->raiz = NULL;
+}
+
+/**
+Crea la raiz de A.
+Si A no es vacio, finaliza indicando ARB_OPERACION_INVALIDA.
+**/
 void crear_raiz(tArbol arbol, tElemento elemento) {
-    if (arbol->raiz == NULL) {
+    if (arbol->raiz != NULL) { //si la raiz no es nula, el arbol no esta vacio
         exit(ARB_OPERACION_INVALIDA);
     }
 
@@ -20,9 +41,16 @@ void crear_raiz(tArbol arbol, tElemento elemento) {
 
     arbol->raiz->elemento = elemento;
     arbol->raiz->padre = NULL;
-    crear_lista(&arbol->raiz->hijos);
+    crear_lista(&(arbol->raiz->hijos));
 }
 
+/**
+ Inserta y retorna un nuevo nodo en A.
+ El nuevo nodo se agrega en A como hijo de NP, hermano izquierdo de NH, y cuyo rotulo es E.
+ Si NH es NULL, el nuevo nodo se agrega como ultimo hijo de NP.
+ Si NH no corresponde a un nodo hijo de NP, finaliza indicando ARB_POSICION_INVALIDA.
+ NP direcciona al nodo padre, mientras NH al nodo hermano derecho del nuevo nodo a insertar.
+**/
 tNodo a_insertar(tArbol arbol, tNodo nodoPadre, tNodo nodoHermano, tElemento elemento) {
     tNodo nodoNuevo;
     if (nodoHermano != NULL) {
@@ -32,7 +60,7 @@ tNodo a_insertar(tArbol arbol, tNodo nodoPadre, tNodo nodoHermano, tElemento ele
 
         nodoNuevo = crear_nodo(nodoPadre, elemento);
 
-        tPosicion posicionHermano = es_hijo_de(nodoHermano);
+        tPosicion posicionHermano = obtenerPosicion(nodoHermano);
 
         l_insertar(nodoPadre->hijos, posicionHermano, nodoNuevo);
     } else {
@@ -44,9 +72,57 @@ tNodo a_insertar(tArbol arbol, tNodo nodoPadre, tNodo nodoHermano, tElemento ele
     return nodoNuevo;
 }
 
+/**
+ Elimina el nodo N de A.
+ El elemento almacenado en el arbol es eliminado mediante la funcion fEliminar parametrizada.
+ Si N es la raiz de A, y tiene un solo hijo, este pasa a ser la nueva raiz del arbol.
+ Si N es la raiz de A, y a su vez tiene mas de un hijo, finaliza retornando ARB_OPERACION_INVALIDA.
+ Si N no es la raiz de A y tiene hijos, estos pasan a ser hijos del padre de N, en el mismo orden y a partir de la posicion que ocupa N en la lista de hijos de su padre.
+**/
+void a_eliminar(tArbol a, tNodo n, void (*fEliminar)(tElemento)){
+    int cantHijos = l_longitud(n->hijos);
+
+    if(n == a->raiz){ //n es raiz
+
+        if(cantHijos == 0){
+            a->raiz=NULL;
+        }
+
+        else if(cantHijos == 1){
+            tNodo nuevaRaiz = l_recuperar(n->hijos, l_primera(n->hijos));
+            a->raiz = nuevaRaiz;
+            nuevaRaiz->padre = NULL;
+        }
+
+        else if(cantHijos > 1){
+            exit(ARB_OPERACION_INVALIDA);
+        }
+    }
+
+    else{ //n no es raiz
+        tLista hermanos = n->padre->hijos;
+        tPosicion hijo = l_primera(n->hijos);
+        tPosicion posN = obtenerPosicion(n);
+        for(int i=0; i<cantHijos; i++){
+            l_insertar(hermanos, posN, l_recuperar(hermanos, hijo));
+        }
+    }
+
+    fEliminar(n->elemento);
+    n->hijos = NULL;
+    n->padre = NULL;
+    free(n);
+
+    //n no puede destruir su lista de hijos xq se eliminan los tElementos
+}
+
+/**
+ Destruye el arbol A, eliminando cada uno de sus nodos.
+ Los elementos almacenados en el arbol son eliminados mediante la funcion fEliminar parametrizada.
+**/
 void a_destruir(tArbol * a, void (*fEliminar)(tElemento)) {
     fEliminarUsuario = fEliminar;
-    
+
     tLista hijos = (*a)->raiz->hijos;
     l_destruir(&hijos, &eliminar_nodo);
 
@@ -62,29 +138,69 @@ void a_destruir(tArbol * a, void (*fEliminar)(tElemento)) {
     fEliminarUsuario = NULL;
 }
 
+/**
+Recupera y retorna el elemento del nodo N.
+*/
+tElemento a_recuperar(tArbol a, tNodo n){
+    return n->elemento;
+}
+
+/**
+Recupera y retorna el nodo correspondiente a la raiz de A.
+**/
 tNodo a_raiz(tArbol a) {
     return a->raiz;
 }
 
+/**
+ Obtiene y retorna una lista con los nodos hijos de N en A.
+**/
 tLista a_hijos(tArbol arbol, tNodo nodo) {
     return nodo->hijos;
 }
 
+/**
+ Inicializa un nuevo arbol en *SA.
+ El nuevo arbol en *SA se compone de los nodos del subarbol de A a partir de N.
+ El subarbol de A a partir de N debe ser eliminado de A.
+**/
+void a_sub_arbol(tArbol a, tNodo n, tArbol * sa){
+    crear_arbol(sa);
+    (*sa)->raiz = n;
+
+    if(n->padre != NULL){
+        tLista hermanos = n->padre->hijos;
+        tPosicion nPos = obtenerPosicion(n);
+        l_eliminar(hermanos, nPos, &fEliminarAux);
+        n->padre == NULL;
+    }
+
+    else{
+        a->raiz == NULL;
+    }
+}
+
+
 // Funciones privadas
 
-tPosicion es_hijo_de(tNodo nodoHermano) {
-    tLista hijosPadre = nodoHermano->padre->hijos;
+/**
+ Retorna la posicion correspondiente al nodo N relativa a la lista de hijos de su padre
+*/
+tPosicion obtenerPosicion(tNodo n) {
+    tLista hijosPadre = n->padre->hijos;
     tPosicion cursor = l_primera(hijosPadre);
 
     while (cursor != l_fin(hijosPadre)) {
         tNodo hijo = (tNodo) l_recuperar(hijosPadre, cursor);
 
-        if (hijo == nodoHermano) {
+        if (hijo == n) {
             return cursor;
         }
 
         cursor = l_siguiente(hijosPadre, cursor);
     }
+
+    return NULL;
 }
 
 void eliminar_nodo(tElemento elemento) {
@@ -99,6 +215,7 @@ void eliminar_nodo(tElemento elemento) {
 }
 
 tNodo crear_nodo(tNodo nodoPadre, tElemento elemento) {
+
     tNodo nodoNuevo = (tNodo) malloc(sizeof(struct nodo));
 
     if (nodoNuevo == NULL) {
@@ -107,7 +224,11 @@ tNodo crear_nodo(tNodo nodoPadre, tElemento elemento) {
 
     nodoNuevo->padre = nodoPadre;
     nodoNuevo->elemento = elemento;
-    crear_lista(&nodoNuevo->hijos);
+    crear_lista(&(nodoNuevo->hijos));
 
     return nodoNuevo;
+}
+
+void fEliminarAux(tElemento e){
+
 }
