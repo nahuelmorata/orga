2ef8c6675c828e12251bd9211e8fbb05f114514b
Matias Nahuel Morata <nahuelmorata@gmail.com>
2022-09-13 11:48:14 -0300

    Feature: Agrego archivo de constantes

---
+++ constantes.h
@@ -0,0 +1,34 @@
+#ifndef CONST_H
+#define CONST_H
+
+/**
+ * Tipo boolean
+ */
+typedef int boolean;
+
+/**
+ * Valor Falso
+ */
+#define FALSE 0
+
+/**
+ * Valor True
+ */
+#define TRUE 1
+
+/**
+ * Cola con prioridad no inicializada
+ */
+#define CCP_NO_INI 2
+
+/**
+ * Posicion nula
+ */
+#define POS_NULA NULL
+
+/**
+ * Elemento nulo
+ */
+#define ELE_NULO NULL
+
+#endif
