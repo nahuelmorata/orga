8db0d27fdb42a134c9c4ab7f865730b10a9ba1fa
VillarretaFlorencia <112629043+VillarretaFlorencia@users.noreply.github.com>
2022-09-22 14:12:43 -0300

    Add files via upload

---
+++ colacp.h
@@ -0,0 +1,57 @@
+typedef struct cola_con_prioridad {
+	int cantidad_elementos;
+	TNodo raiz;
+	int (*comparador)(TEntrada, TEntrada);
+} * TColaCP;
+
+typedef struct nodo {
+	TEntrada entrada;
+	struct nodo * padre;
+	struct nodo * hijo_izquierdo;
+	struct nodo * hijo_derecho;
+} * TNodo;
+
+typedef struct entrada {
+	TClave clave;
+	TValor valor;
+} * TEntrada;
+
+typedef void * TClave;
+typedef void * TValor;
+
+
+/*
+    * Crea una cola con prioridad
+    * @param f, una funcion de ordenamiento
+    * @return una cola con prioridad vacia
+*/
+TColaCP crear_cola_cp(int (*f)(TEntrada, TEntrada));
+
+/*
+    * Inserta una entrada en la cola con prioridad
+    * @param cola, una cola con prioridad
+    * @param entr entrada a insertar
+    * @return verdadero si procede con ´exito, falso en caso contrario
+*/
+int cp_insertar(TColaCP cola, TEntrada entr);
+
+/*
+	* Elimina la entrada con mayor prioridad
+    * @param cola, una cola con prioridad
+    * @return  la entrada eliminada, o ELE NULO si la cola es vacia
+*/
+TEntrada cp_eliminar(TColaCP cola);
+
+
+/*
+    * Retorna la cantidad de entradas
+    * @return cantidad de entradas
+*/
+int cp_cantidad(TColaCP cola);
+
+/*
+	* Elimina todas las entradas y libera toda la memoria utilizada por la cola
+	* @param cola, una cola con prioridad
+    * @param fEliminar, una funcion de  eliminacion
+*/
+void cp_destruir(TColaCP cola, void (*fEliminar)(TEntrada) );
