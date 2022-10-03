0c00bbd7a7812261e655f53875c77d86a01d6605
Matias Nahuel Morata <nahuelmorata@gmail.com>
2022-10-02 22:16:42 -0300

    Feature: Agrego lectura del archivo a array de ciudad

--- main.c
+++ main.c
@@ -1,76 +1,88 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "colacp/colacp.h"
-#include "constantes.h"
+#include <stdlib.h>
+#include <string.h>
+#include "planificador/planificador.h"
 
-int comparador(TEntrada entrada1, TEntrada entrada2) {
-    int clave1 = *((int *) entrada1->clave);
-    int clave2 = *((int *) entrada2->clave);
-    if (clave1 < clave2) {
-        return 1;
+TCiudad *agrandar_arreglo(TCiudad ciudades[], int cantidad) {
+    TCiudad *ciudades_aux = (TCiudad *) malloc(sizeof(TCiudad) * cantidad);
+    for (int i = 0; i < cantidad - 1; i++) {
+        ciudades_aux[i] = ciudades[i];
     }
-    if (clave2 < clave1) {
-        return -1;
+    free(ciudades);
+    return ciudades_aux;
+}
+
+void leer_nombre(TCiudad ciudad, FILE *archivo) {
+    char nombre_final[100];
+    int caracter_leido = fgetc(archivo);
+    unsigned int contador = 0;
+
+    while (caracter_leido != ';' && contador < 100) {
+        nombre_final[contador] = (char) caracter_leido;
+
+        contador++;
+        caracter_leido = fgetc(archivo);
     }
+
+    ciudad->nombre = (char *) malloc(sizeof(char) * (contador + 1));
+
+    ciudad->nombre[contador] = '\0';
+    memcpy(ciudad->nombre, nombre_final, contador);
+}
+
+
+TCiudad* leer_archivo(char *path_archivo, int *cantidad_elementos) {
+    TCiudad *ciudades = (TCiudad *) malloc(sizeof(TCiudad) * (*cantidad_elementos));
+    FILE *archivo_ciudades = fopen(path_archivo, "r");
+    int indice = 0;
+    TCiudad ciudad_leida;
+
+    while(!feof(archivo_ciudades)) {
+        if (*cantidad_elementos == indice) {
+            ciudades = agrandar_arreglo(ciudades, *cantidad_elementos + 1);
+            *cantidad_elementos = *cantidad_elementos + 1;
+        }
+        ciudades[indice] = (TCiudad) malloc(sizeof(struct ciudad));
+        ciudad_leida = ciudades[indice];
+        if (indice == 0) {
+            ciudad_leida->nombre = NULL;
+            fscanf(archivo_ciudades, "%f;%f\n", &ciudad_leida->pos_x, &ciudad_leida->pos_y);
+        } else {
+            leer_nombre(ciudad_leida, archivo_ciudades);
+            fscanf(archivo_ciudades, "%f;%f\n", &ciudad_leida->pos_x, &ciudad_leida->pos_y);
+        }
+        indice++;
+        printf("\n");
+    }
+
+    fclose(archivo_ciudades);
+    return ciudades;
+}
+
+void liberar_ciudades(TCiudad *ciudades, int cantidad_elementos) {
+    for(int i = 0; i < cantidad_elementos; i++) {
+        if (ciudades[i]->nombre != NULL) {
+            free(ciudades[i]->nombre);
+        }
+        free(ciudades[i]);
+    }
+    free(ciudades);
+}
+
+int main(int argc, char* args[]) {
+    if (argc < 2) {
+        printf("Falta el archivo de viajes. El formato es %s <archivo>", args[0]);
+        return 0;
+    }
+
+    int cantidad_elementos = 2;
+    TCiudad *ciudades = leer_archivo(args[1], &cantidad_elementos);
+
+    for(int i = 0; i < cantidad_elementos; i++) {
+        TCiudad ciudad = ciudades[i];
+        printf("%s en x = %f, y = %f\n", ciudad->nombre, ciudad->pos_x, ciudad->pos_y);
+    }
+    liberar_ciudades(ciudades, cantidad_elementos);
+
     return 0;
-}
-
-void eliminar_entrada(TEntrada entrada) {
-    free(entrada->clave);
-    free(entrada->valor);
-    free(entrada);
-}
-
-TEntrada generar_entrada(int clave, int valor) {
-    TEntrada entrada = (TEntrada) malloc(sizeof(struct entrada));
-    int *clavePuntero = (int*) malloc(sizeof(int));
-    int *claveValor = (int*) malloc(sizeof(int));
-    *clavePuntero = clave;
-    *claveValor = valor;
-    entrada->clave = clavePuntero;
-    entrada->valor = claveValor;
-    return entrada;
-}
-
-void mostrar_nodo(TNodo nodo) {
-    printf("Padre: %d\n", *(int*)(nodo->entrada->clave));
-    if (nodo->hijo_izquierdo != ELE_NULO) {
-        printf("Hijo izquierdo: %d\n", *(int*)(nodo->hijo_izquierdo->entrada->clave));
-    }
-    if (nodo->hijo_derecho != ELE_NULO) {
-        printf("Hijo derecho: %d\n", *(int*)(nodo->hijo_derecho->entrada->clave));
-    }
-
-    if (nodo->hijo_izquierdo != ELE_NULO) {
-        mostrar_nodo(nodo->hijo_izquierdo);
-    }
-    if (nodo->hijo_derecho != ELE_NULO) {
-        mostrar_nodo(nodo->hijo_derecho);
-    }
-}
-
-void mostrar_cola(TColaCP cola) {
-    mostrar_nodo(cola->raiz);
-}
-
-int main() {
-    TColaCP cola = crear_cola_cp(comparador);
-
-    cp_insertar(cola, generar_entrada(20, 1));
-    cp_insertar(cola, generar_entrada(45, 2));
-    cp_insertar(cola, generar_entrada(30, 3));
-    cp_insertar(cola, generar_entrada(16, 4));
-    cp_insertar(cola, generar_entrada(18, 5));
-    cp_insertar(cola, generar_entrada(33, 6));
-    cp_insertar(cola, generar_entrada(25, 7));
-
-    mostrar_cola(cola);
-
-    TEntrada entrada = cp_eliminar(cola);
-    printf("Entrada eliminada: %i", *((int *)entrada->clave));
-    eliminar_entrada(entrada);
-
-    mostrar_cola(cola);
-
-    cp_destruir(cola, eliminar_entrada);
 }
---
+++ planificador/planificador.h
@@ -0,0 +1,5 @@
+typedef struct ciudad {
+    char * nombre;
+    float pos_x;
+    float pos_y;
+} * TCiudad;
